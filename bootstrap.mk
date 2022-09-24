792662cc692fd3fafe5e4af1b7edac03d7b0a7e6
Pat Maddox <pat@patmaddox.com>
2022-09-24 13:23:11 -0700

    it's just a cooler name

--- bootstrap.mk
+++ bootstrap.mk
@@ -1,14 +1,14 @@
-all: devbox.git
+all: system0.git
 
-.PHONY: sudo git devbox.git
+.PHONY: sudo git system0.git
 
 # Defaults to my private repo, but can override with
-# https://github.com/patmaddox/devbox.git
-REPO := git@github.com:patmaddox/devbox.git
+# https://github.com/patmaddox/system0.git
+REPO := git@github.com:patmaddox/system0.git
 
 # shouldn't need to check for dir, but for some reason this gets run even ater cloning
-devbox.git: $(HOME)/devbox.git
-$(HOME)/devbox.git: git
+system0.git: $(HOME)/system0.git
+$(HOME)/system0.git: git
 	if [ ! -d $(.TARGET) ]; then git clone --bare $(REPO) $(.TARGET); fi
 
 git: /usr/local/bin/git
